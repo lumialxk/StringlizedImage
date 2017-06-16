be11c240e590912fbf4c79b713b52e2e998dc5a7
李现科 <lumialxk@outlook.com>
2017-06-16 17:17:25 +0800

    Initial Commit

---
+++ StringlizedImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringlizedImage
+//
+//  Created by 李现科 on 2017/6/16.
+//  Copyright © 2017年 李现科. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
